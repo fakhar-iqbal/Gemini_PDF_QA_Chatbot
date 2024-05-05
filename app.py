d292ddda874e82c547cce4a3eecfebee954a94d6
Muhammad Fakhar-e-Iqbal Abdullah <fakhareiqbal3534@gmail.com>
2024-05-05 21:39:53 +0500

    update

--- app.py
+++ app.py
@@ -63,3 +63,42 @@ def get_conversational_chain():
     prompt=PromptTemplate(template=prompt_template,input_variables=["context","question"])
     chain=load_qa_chain(model,chain_type="stuff",prompt=prompt)
     return chain
+
+def user_input(user_question):
+    embeddings = GoogleGenerativeAIEmbeddings(model="model/embedding-001")
+    new_db = FAISS.load_local("faiss_index",embeddings)
+    docs = new_db.similarity_search(user_question)
+    chain = get_conversational_chain()
+    
+    response = chain(
+        {
+            "input_documents":docs,"question":user_question
+        },return_only_outputs=True
+    )
+    print(response)
+    st.write("Reply: ", response['output_text'])
+    
+def main():
+    st.set_page_config("Chat with Multiple pdfs")
+    st.header("chat with multiple PDFs using Gemini")
+    
+    user_question  = st.text_input("Ask a question from your pdf files")
+    
+    if user_question:
+        user_input(user_question)
+        
+    with st.sidebar:
+        st.title("Menu:")
+        pdf_docs = st.file_uploader("upload yur pdf file and click submit & process")
+        
+        if st.button("Submit & Process"):
+            with st.spinner("Processing..."):
+                raw_text = get_pdf_text(pdf_docs)
+                text_chunks = get_text_chunks(raw_text)
+                get_vector_store(text_chunks)
+                st.success("Done")
+    
+    
+
+if __name__=="__main__":
+    main()
